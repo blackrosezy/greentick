7e0e4ead3ddef89cd0665f44402aa3593fc3c5c3
Mohd Rozi <blackrosezy@gmail.com>
2013-05-23 12:04:17 +0800

    Add IEs

--- fetch.bat
+++ fetch.bat
@@ -37,7 +37,7 @@ ren version.json.1 version.json
 REM ------------------------------
 REM Oracle Java
 REM ------------------------------
-echo Get Mozilla Firefox latest version
+echo Get Oracle Java latest version
 
 xidel http://java.com/en/download/chrome.jsp?locale=en -e "//a[@class='jvdla0']/@href" > tmp_java_file
 set /p url_java=<tmp_java_file
@@ -50,7 +50,7 @@ ren version.json.1 version.json
 REM ------------------------------
 REM Google Chrome
 REM ------------------------------
-echo Get Mozilla Firefox latest version
+echo Get Google Chrome latest version
 
 wget --no-check-certificate https://tools.google.com/service/update2 --post-data "<?xml version='1.0' encoding='UTF-8'?><request protocol='3.0' version='1.3.21.135' shell_version='1.3.21.103' ismachine='1' sessionid='{8989B147-F32D-43EA-BB06-9901D388E712}' installsource='ondemandcheckforupdate' requestid='{4A6167C6-5DDA-435C-B306-D591CA20FB0D}'><os platform='win' version='6.1' sp='' arch='x86'/><app appid='{4DC8B4CA-1BDA-483E-B5FA-D3C12E15B62D}' version='0' nextversion='' ap='-multi-chrome' lang='' brand='GGLS' client=''><updatecheck/><ping active='1'/></app></request>" -O tmp_chrome.xml
 REM tutorial about XMLStarlet http://dynamomd.org/index.php/tutorialA
@@ -63,18 +63,52 @@ ren version.json.1 version.json
 REM ------------------------------
 REM Microsoft Internet Explorer for XP
 REM ------------------------------
-echo Get Mozilla Firefox latest version
+echo Get Microsoft Internet Explorer for XP latest version
 
-xidel http://windows.microsoft.com/en-us/windows/upgrade-your-browser -e "//span[@class='btnBase']/a/@href" > tmp_ie_xp_file
+REM Minimum user agent for MSIE 6 (Win XP) is "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)"
+xidel http://windows.microsoft.com/en-MY/internet-explorer/download-ie --user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)" -e "(//span[@class='btnBase']/a/@href)[1]" > tmp_ie_xp_file
 set /p url_ie_xp=<tmp_ie_xp_file
-wget --no-check-certificate "%url_ie_xp%" -O tmp_ie_xp.exe
+wget --user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)" "%url_ie_xp%" -O tmp_ie_xp.exe
 7za.exe e tmp_ie_xp.exe -y -otmp_ie_xp
 for /f "tokens=3" %%v in ('sigcheck "tmp_ie_xp/iexplore.exe"^|find /i "version"') do set fileVersion=%%v
 jq ".ie_xp = \"%fileVersion%\"" version.json > version.json.1
 del version.json
 ren version.json.1 version.json
 
+REM ------------------------------
+REM Microsoft Internet Explorer for Vista
+REM ------------------------------
+echo Get Microsoft Internet Explorer for Vista latest version
 
-REM xidel http://www.microsoft.com/windows/ie/ --user-agent="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)" -e "(//span[@class='btnBase']/a/@href)[1]" > tmp_ie_vista
-REM set /p url_ie_vista=<tmp_ie_vista
-REM wget --user-agent="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)" "%url_ie_vista%" -O ie_vista.html
+REM Minimum user agent for MSIE 7 (Vista) is "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)"
+xidel http://windows.microsoft.com/en-MY/internet-explorer/download-ie --user-agent="Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)" -e "(//span[@class='btnBase']/a/@href)[1]" > tmp_ie_vista_file
+set /p url_ie_vista=<tmp_ie_vista_file
+wget --user-agent="Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)" "%url_ie_vista%" -O tmp_ie_vista.exe
+for /f "tokens=3" %%v in ('sigcheck "tmp_ie_vista.exe"^|find /i "version"') do set fileVersion=%%v
+jq ".ie_vista = \"%fileVersion%\"" version.json > version.json.1
+del version.json
+ren version.json.1 version.json
+
+
+REM ------------------------------
+REM Microsoft Internet Explorer for Windows 7 and 8
+REM ------------------------------
+echo Get Microsoft Internet Explorer for Windows 7 ad 8 latest version
+
+REM Minimum user agent for MSIE 8 - standard mode (Win 7) is "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)"
+xidel http://windows.microsoft.com/en-MY/internet-explorer/download-ie --user-agent="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)" -e "(//span[@class='btnBase']/a/@href)[1]" > tmp_ie_win7_file
+set /p url_ie_win7=<tmp_ie_win7_file
+wget --user-agent="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)" "%url_ie_win7%" -O tmp_ie_win7.exe
+for /f "tokens=3" %%v in ('sigcheck "tmp_ie_win7.exe"^|find /i "version"') do set fileVersion=%%v
+jq ".ie_win7 = \"%fileVersion%\"" version.json > version.json.1
+jq ".ie_win8 = \"%fileVersion%\"" version.json.1 > version.json.2
+del version.json
+del version.json.1
+ren version.json.2 version.json
+
+
+REM cleanup temporary files/folders
+rm -rf tmp_*
+rm -rf *.txt
+rm -rf *.xml
+
